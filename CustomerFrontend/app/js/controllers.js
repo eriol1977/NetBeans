262cb0d6453f6e2e2e98f0f7832266b2acde0d36
francesco <francesco@wplex.com.br>
2014-02-28 11:43:30 -0300

    alterações

--- CustomerFrontend/app/js/controllers.js
+++ CustomerFrontend/app/js/controllers.js
@@ -7,4 +7,25 @@ angular.module('myApp.controllers', []).
           {
               $scope.selected = customer;
           };
+          
+          $scope.insertCustomer = function() {
+              var customer = new Customers({
+                  customerId: "777",
+                  name: "Tony Manero SPA",
+                  city: "Cincinnati",
+                  state: "SC"
+              });
+              customer.$save(); 
+          };
+          
+          $scope.deleteCustomer = function(customer) {
+              Customers.delete({id: customer.customerId});
+              //$scope.allCustomers = Customers.query();
+          };
+          
+          $scope.getCustomer = function(id) {
+              if(id === undefined)
+                  id = "1";
+              $scope.gotCustomer = Customers.get({id:id});
+          };
   });
--- CustomerFrontend/app/js/services.js
+++ CustomerFrontend/app/js/services.js
@@ -3,7 +3,7 @@
 var customerServices = angular.module('myApp.services', ['ngResource']);
 
 customerServices.factory('Customers', function($resource){
-    return $resource('http://localhost:8080/CustomerBackend/webresources/customers',{},{
+    return $resource('http://localhost:8080/CustomerBackend/webresources/customers/:id',{},{
         query: {method:'GET',isArray:true}
     });
 });
