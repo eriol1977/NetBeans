d2a97f2e2293ad347b01938227deb9724a606365
francesco <francesco@wplex.com.br>
2014-03-06 12:59:16 -0300

    adicionado teste unitário

--- AngularTests/test/unit/controllersSpec.js
+++ AngularTests/test/unit/controllersSpec.js
@@ -1,16 +1,15 @@
-'use strict';
+describe('ListaCtrl', function() {
+    var scope;
+    var ctrl;
+    beforeEach(module("myApp.controllers"));
+    beforeEach(inject(function($rootScope, $controller) {
+        scope = $rootScope.$new();
+        ctrl = $controller("ListaCtrl", {$scope: scope});
+    }));
 
-/* jasmine specs for controllers go here */
-
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
-
-
-  it('should ....', inject(function() {
-    //spec body
-  }));
-
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    it("should have SC as first state", function() {
+        expect(scope.states[0].code).toBe('SC');
+        expect(scope.states[0].name).toBe('Santa Catarina');
+        expect(scope.states[0].region).toBe('South');
+    });
 });
