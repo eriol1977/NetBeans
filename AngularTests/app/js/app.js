7aa31bdc7101cc4c50e0bb86290ad018e45fa86f
francesco <francesco@wplex.com.br>
2014-03-12 10:35:47 -0300

    pagination directive with tests

--- AngularTests/app/js/app.js
+++ AngularTests/app/js/app.js
@@ -14,5 +14,6 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {templateUrl: 'partials/validacao.html', controller: 'ValidacaoCtrl'});
   $routeProvider.when('/view3', {templateUrl: 'partials/password.html', controller: 'PasswordCtrl'});
   $routeProvider.when('/view4/:itemId', {templateUrl: 'partials/item.html', controller: 'ItemCtrl'});
+  $routeProvider.when('/view5', {templateUrl: 'partials/pagination.html', controller: 'PaginationCtrl'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- AngularTests/app/js/controllers.js
+++ AngularTests/app/js/controllers.js
@@ -3,44 +3,47 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('ListaCtrl', function($scope) {
-          $scope.states = [
-              {code: 'SC',
-               name: 'Santa Catarina',
-               region: 'South'},
-              {code: 'RS',
-               name: 'Rio Grande do Sul',
-               region: 'South'},
-              {code: 'BA',
-               name: 'Bahia',
-               region: 'North East'},
-              {code: 'CE',
-               name: 'Ceará',
-               region: 'North East'}
-          ];
-  })
-  .controller('ValidacaoCtrl', function($scope) {
-      $scope.getCssClasses = function(ngModelController) {
-        return {
-        'form-group has-error': ngModelController.$invalid,
-        'form-group has-success': ngModelController.$valid
-        };
-      };
-      $scope.showError = function(ngModelController, error) {
-        return ngModelController.$error[error];
-      };
-  }).controller('PasswordCtrl', function($scope) {
-      
-  }).controller('ItemCtrl', function($scope, $routeParams) {
+        controller('ListaCtrl', function($scope) {
+            $scope.states = [
+                {code: 'SC',
+                    name: 'Santa Catarina',
+                    region: 'South'},
+                {code: 'RS',
+                    name: 'Rio Grande do Sul',
+                    region: 'South'},
+                {code: 'BA',
+                    name: 'Bahia',
+                    region: 'North East'},
+                {code: 'CE',
+                    name: 'Ceará',
+                    region: 'North East'}
+            ];
+        })
+        .controller('ValidacaoCtrl', function($scope) {
+            $scope.getCssClasses = function(ngModelController) {
+                return {
+                    'form-group has-error': ngModelController.$invalid,
+                    'form-group has-success': ngModelController.$valid
+                };
+            };
+            $scope.showError = function(ngModelController, error) {
+                return ngModelController.$error[error];
+            };
+        }).controller('PasswordCtrl', function($scope) {
+
+}).controller('ItemCtrl', function($scope, $routeParams) {
     $scope.items = [
-        {id: '1', name: 'Forchetta'},  
-        {id: '2', name: 'Coltello'}  
+        {id: '1', name: 'Forchetta'},
+        {id: '2', name: 'Coltello'}
     ];
     $scope.getItemById = function(id) {
-        for(var i=0;i<$scope.items.length;i++)  {
-          if($scope.items[i].id === id)
-              return $scope.items[i];
-      }
+        for (var i = 0; i < $scope.items.length; i++) {
+            if ($scope.items[i].id === id)
+                return $scope.items[i];
+        }
     };
     $scope.selectedItem = $scope.getItemById($routeParams.itemId);
-  });
+}).controller('PaginationCtrl', function($scope) {
+    $scope.numPages = 7;
+    $scope.currentPage = 3;
+});
